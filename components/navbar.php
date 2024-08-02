1e26aeeea8b8bbcd539521f2df294305c04da1d1
Romildo Jr <fis.romildojr@gmail.com>
2024-08-02 09:34:56 -0300

    Ajuste

--- components/navbar.php
+++ components/navbar.php
@@ -6,7 +6,7 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#""hom""e">Home <span class="sr-only">(Página atual)</span></a>
+        <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="?pg=categorias">Categorias</a>
